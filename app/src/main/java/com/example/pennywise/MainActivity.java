aa604bf8382690d5c18f0260a4bc1b738228380f
Tpstorer <121822016+Tpstorer@users.noreply.github.com>
2024-05-29 20:34:46 -0400

    Bottom nav

--- app/src/main/java/com/example/pennywise/MainActivity.java
+++ app/src/main/java/com/example/pennywise/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.pennywise;
 
 import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
@@ -13,24 +17,50 @@ import com.example.pennywise.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import org.jetbrains.annotations.NotNull;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+     BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main);
 
         setSupportActionBar(binding.toolbar);
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        //getSupportFragmentManager().beginTransaction().replace(R.id.main_container, new FirstFragment()).commit();
+        bottomNavigationView.setSelectedItemId(R.id.nav_home);
+
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+              public boolean onNavigationItemSelected(@NonNull  MenuItem item) {
+                Fragment fragment = null;
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        fragment = new FirstFragment();
+                        break;
+                    case R.id.nav_goals:
+                        fragment = new TestFragment();
+                        break;
+                    case R.id.nav_bills:
+                        fragment = new SecondFragment();
+                        break;
+                }
+
+                return true;
+            }
+        });
 
     }
 
@@ -62,4 +92,7 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+
+
 }
